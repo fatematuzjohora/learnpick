5fbf6ebeaf3340823213babbcd8361afd6f46811
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-04-19 15:55:44 +0600

    update

---
+++ src/component/Pages/AboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div className='w-50 mx-auto my-5'>
+            <div className="shadow-lg p-5  bg-body rounded text-center">
+                <h3 className='mt-3 mb-5'>Fatema Tuz-Johora</h3>
+                <p className='text-start'><span className=' fw-bolder'>MY DREAM AND GOALS FOR THE NEXT SIX MONTH:</span> I am learning programming as a student in programming-hero, my dream for the next six months is to establish myself as a web developer and for this I am working as my level best, hopefully in next six months I will be able to fulfill my dream.Keep me in your prayer.</p>
+
+            </div>
+           
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/component/Pages/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div>
+            <h1 className='text-center'>Thanks for purchase my class</h1>
+        </div>
+    );
+};
+
+export default Checkout;
