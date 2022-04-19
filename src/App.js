1077805485dc1807ea19b870b1146ec65541ea5e
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-04-19 15:25:41 +0600

    app-js done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Shared/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Pages/Home/Home/Home";
+import Footer from "./Components/Shared/Footer/Footer";
+import AboutMe from "./Components/Pages/AboutMe/AboutMe";
+import Login from "./Components/Pages/Login/Login/Login";
+import Register from "./Components/Pages/Login/Register/Register";
+import NotFoundPage from "./Components/Shared/NotFoundPage/NotFoundPage";
+import Blogs from "./Components/Pages/Blogs/Blogs";
+import Checkout from "./Components/Pages/Checkout/Checkout"
+import RequireAuth from "./Components/Pages/Login/RequireAuth/RequireAuth";
+import Services from "./Components/Pages/Home/Services/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/aboutMe" element={<AboutMe></AboutMe>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/checkout" element={
+        <RequireAuth>
+          <Checkout></Checkout>
+        </RequireAuth>
+        }></Route>
+        <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
 
 export default App;
+
+
